6f36eb764142e25b1a632367d0b37d340a624367
Yuzhen-Cai <caiyuzhen@cspid.cn>
2026-08-15 15:58:08 +0800

    Upload openpi, realtime inference and rlinf work

---
+++ infra/RLinf/run_train.py
@@ -0,0 +1,15 @@
+# pi0 ---> PPO
+# vim examples/embodiment/config/libero_spatial_ppo_openpi_quickstart.yaml
+# bash examples/embodiment/run_embodiment.sh libero_spatial_ppo_openpi_quickstart
+
+# pi0.5 ---> PPO
+# vim examples/embodiment/config/libero_spatial_ppo_openpi_pi05_quickstart.yaml
+# bash examples/embodiment/run_embodiment.sh libero_spatial_ppo_openpi_pi05_quickstart
+
+
+# pi0.5 ---> GRPO
+# vim examples/embodiment/config/libero_spatial_grpo_openpi_pi05_quickstart.yaml
+bash examples/embodiment/run_embodiment.sh libero_spatial_grpo_openpi_pi05_quickstart
+
+
+
